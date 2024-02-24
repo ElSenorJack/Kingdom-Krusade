152893d39faab858c469b1f2f8fe454fff4c619d
Giacomo "Jack" Pedemonte <160411435+ElSenorJack@users.noreply.github.com>
2024-02-24 17:47:16 +0100

    Improved enemy and turret
    Added health, targeting and tower damage

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int maxHP = 5;
+    int currentHP = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    // Update is called once per frame
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        currentHP--;
+
+        if (currentHP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
