e4f6379ed688b487e00ca7a658e1b44ba04fa650
Giacomo "Jack" Pedemonte <160411435+ElSenorJack@users.noreply.github.com>
2024-02-24 16:51:19 +0100

    Improved Scripts
    Added Vector3.Lerp to 'Enemy'
    Added Mouse Input to 'Waypoint'

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range (0f, 5f)] float speed = 1f; //Limitiamo la velocità perchè valori negativi darebbero problemi all'equazione e troppo veloce sarebbe brutto
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +17,18 @@ public class EnemyMover : MonoBehaviour
     {
         foreach (Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
 
-            yield return new WaitForSeconds(waitTime); //il return rimanda la lettura del codice dopo 1 secondo, tornando a vv
-                                                 // Start> CalLmethod> IEnum legge il primo valore> aspetta 1 sec > Start
+            transform.LookAt(endPosition); //per ruotare la pedina in direzione del movimento
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame(); //con Yield viene fatta riniziare questa fase in modo ciclico fino alla risoluzione del movimento
+            }  
         }
     }
 }
